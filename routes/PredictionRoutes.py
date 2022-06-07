1cfcb335e1cb61d86560d73f14ad18b824f3644d
startups <konstantinos.xr96@gmail.com>
2022-06-07 23:46:39 +0300

    fix in routes

--- routes/PredictionRoutes.py
+++ routes/PredictionRoutes.py
@@ -8,32 +8,32 @@ route1 = Blueprint('route1', __name__)
 
 
 @route1.route('/model1a')
-def model1():
+def model1a():
     return jsonify(1)
 
 
 @route1.route('/model1b')
-def model1():
+def model1b():
     return jsonify(1)
 
 
 
 @route1.route('/model2a')
-def model2():
+def model2a():
     return jsonify(2)
 
 
 @route1.route('/model2b')
-def model2():
+def model2b():
     return jsonify(2)
 
 
 
 @route1.route('/model3a')
-def model3():
+def model3a():
     return jsonify(3)
 
 
 @route1.route('/model3b')
-def model3():
+def model3b():
     return jsonify(3)

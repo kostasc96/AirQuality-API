2d59bc25e2245da0ab62c92ae3fbc2c42dfd079d
startups <konstantinos.xr96@gmail.com>
2022-06-18 16:12:33 +0300

    reformat project and added util functions for training section

--- util.py
+++ util.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 utils = {}
 util = lambda f: utils.setdefault(f.__name__, f)
 
@@ -36,3 +38,38 @@ def update_model3a(model, acc):
 @util
 def update_model3b(model, acc):
     model.update_model3b(acc)
+
+
+@util
+def convert_pm10_index(predicts, test):
+    for i in range(len(predicts)):
+        for j in range(len(predicts[i])):
+            predicts[i][j] = pm10_index(predicts[i][j])
+            test[i][j] = pm10_index(test[i][j])
+
+
+@util
+def calc_pm10_accuracy(predicts, test, window):
+    tot_val = len(predicts) * window
+    tot_comp = 0
+    for i in range(len(predicts)):
+        for j in range(window):
+            if abs(predicts[i][j] - test[i][j]) == 1:
+                tot_comp = tot_comp + 0.5
+            elif abs(predicts[i][j] == test[i][j]):
+                tot_comp = tot_comp + 1
+    return (100 * tot_comp) / tot_val
+
+
+@util
+def create_xy(series, series2, window_size, prediction_horizon, shuffle=False):
+    x = []
+    y = []
+    for i in range(0, len(series)):
+        if len(series[(i + window_size):(i + window_size + prediction_horizon)]) < prediction_horizon:
+            break
+        x.append(np.array(series[i:(i + window_size)]))
+        y.append(np.array(series2[(i + window_size):(i + window_size + prediction_horizon)]))
+    x = np.array(x)
+    y = np.array(y)
+    return x, y

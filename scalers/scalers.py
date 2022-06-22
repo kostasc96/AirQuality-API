c80e0384bc1c1d5dddb452fdadfe5113c4419606
startups <konstantinos.xr96@gmail.com>
2022-06-22 23:44:28 +0300

    added fields for scalers for grid model

--- scalers/scalers.py
+++ scalers/scalers.py
@@ -5,6 +5,8 @@ class ScalerDNN:
     def __init__(self, base_path):
         self.scaler1a = joblib.load(f'{base_path}/scaler1a.gz')
         self.scaler1b = joblib.load(f'{base_path}/scaler1b.gz')
+        self.scaler3a = joblib.load(f'{base_path}/scaler3a.gz')
+        self.scaler3b = joblib.load(f'{base_path}/scaler3b.gz')
 
     def get_scaler(self, ver):
         scaler_fn = self.get_scalers(ver)
@@ -15,9 +17,19 @@ class ScalerDNN:
             return self._get_scaler_1a()
         elif ver == "1b":
             return self._get_scaler_1b()
+        elif ver == "3a":
+            return self._get_scaler_3a()
+        else:
+            return self._get_scaler_3b()
 
     def _get_scaler_1a(self):
         return self.scaler1a
 
     def _get_scaler_1b(self):
         return self.scaler1b
+
+    def _get_scaler_3a(self):
+        return self.scaler3a
+
+    def _get_scaler_3b(self):
+        return self.scaler3b

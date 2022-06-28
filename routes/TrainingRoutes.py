145505ae5b73e694b4e185ea1a939cb781bc1a99
startups <konstantinos.xr96@gmail.com>
2022-06-29 00:32:24 +0300

    tested thread function for model online learning

--- routes/TrainingRoutes.py
+++ routes/TrainingRoutes.py
@@ -1,6 +1,8 @@
 from flask import jsonify, Response
 from flask import Blueprint
 from clients.cassandra import get_cassandra_client
+import time
+import threading
 
 session = get_cassandra_client('localhost', 9042, 'airquality')
 
@@ -9,4 +11,10 @@ route2 = Blueprint('route2', __name__)
 
 @route2.route('/online')
 async def online_training():
-    return jsonify({"result":"connection successful"})
+    def thread_function(value):
+        print("Thread %s: starting" + str(value))
+        time.sleep(value)
+        print("Thread %s: finishing" + str(value))
+    thread = threading.Thread(target=thread_function, kwargs={'value': 20})
+    thread.start()
+    return jsonify({"result": "connection successful"})

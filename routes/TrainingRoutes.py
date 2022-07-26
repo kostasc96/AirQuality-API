2dd78799a523654c73177c5d25f2d02c35aa6ce8
startups <konstantinos.xr96@gmail.com>
2022-07-27 01:58:44 +0300

    testing in model training

--- routes/TrainingRoutes.py
+++ routes/TrainingRoutes.py
@@ -3,6 +3,7 @@ from flask import Blueprint
 from clients.cassandra import get_cassandra_client
 import time
 import threading
+from initiators import model
 
 session = get_cassandra_client('localhost', 9042, 'airquality')
 
@@ -11,10 +12,11 @@ route2 = Blueprint('route2', __name__)
 
 @route2.route('/online')
 async def online_training():
-    def thread_function(value):
+    def thread_function(value, con, models):
         print("Thread %s: starting" + str(value))
         time.sleep(value)
+        print(models.get_accuracy_models("1a"))
         print("Thread %s: finishing" + str(value))
-    thread = threading.Thread(target=thread_function, kwargs={'value': 20})
+    thread = threading.Thread(target=thread_function, kwargs={'value': 20, 'con': session, 'models': model})
     thread.start()
     return jsonify({"result": "connection successful"})

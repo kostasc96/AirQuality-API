0f05d3c35f7e02eb32a28af23f6332955a0cd7b7
startups <konstantinos.xr96@gmail.com>
2022-09-20 02:39:33 +0300

    normalization on predictions route

--- routes/PredictionRoutes.py
+++ routes/PredictionRoutes.py
@@ -2,6 +2,8 @@ from flask import jsonify, request
 from flask import Blueprint
 from initiators import model, scalers, prediction, meteo
 from util import utils
+import numpy as np
+import pandas as pd
 
 route1 = Blueprint('route1', __name__)
 
@@ -22,7 +24,15 @@ def model1a():
 @route1.route('/model1b', methods = ['POST'])
 def model1b():
     req = request.get_json()
-    tmpDs = meteo.update_meteo_value(req['station_id'], req['val'])
+    tmpDs = np.array(meteo.update_meteo_value(req['station_id'], req['val']))
+    # tmpDs = pd.DataFrame({'station_id': tmpDs[:, 0], 'date_time': tmpDs[:, 1], 'forecast_tempC': tmpDs[:, 2], 'forecast_humidity': tmpDs[:, 3], 'forecast_windSpeed': tmpDs[:, 4], 'forecast_windDirection': tmpDs[:, 5], 'pm10': tmpDs[:, 6]})
+    cnt = 0
+    for i in tmpDs[0]:
+        j = np.array([[i[2], i[3], i[4], i[5], i[6]]])
+        j = scalers.get_scaler(False, "a").transform(j)
+        tmpDs[0][cnt] = [i[0], i[1], j[0][0], j[0][1], j[0][2], j[0][3], j[0][4]]
+        cnt = cnt + 1
+    print(tmpDs)
     pred = model.get_model("1b").predict(tmpDs)
     preds = scalers.get_scaler(False, "b").inverse_transform(pred.reshape(-1, 1)).reshape(pred.shape)
     return_list = []

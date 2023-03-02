527d835d7007da1c4b9ee943461eceb985dfcd38
startups <konstantinos.xr96@gmail.com>
2023-03-02 02:10:45 +0200

    changed to csv temp file

--- routes/PredictionRoutes.py
+++ routes/PredictionRoutes.py
@@ -21,33 +21,61 @@ def model1a():
     return jsonify(prediction.get_prediction(req['station_id']))
 
 
+# @route1.route('/model1b', methods=['POST'])
+# def model1b():
+#     req = request.get_json()
+#     tmpDs = np.array(meteo.update_meteo_value(req['station_id'], req['val']))
+#     tmpDs = tmpDs[0]
+#     df = pd.DataFrame({'station_id': tmpDs[:, 0], 'date_time': tmpDs[:, 1], 'forecast_tempC': tmpDs[:, 2],
+#                        'forecast_humidity': tmpDs[:, 3], 'forecast_windSpeed': tmpDs[:, 4],
+#                        'forecast_windDirection': tmpDs[:, 5], 'pm10': tmpDs[:, 6]})
+#     df['date_time'] = df['date_time'].astype("|S")
+#     df['date_time'] = df['date_time'].astype(int)
+#     df['forecast_windDirection'] = df['forecast_windDirection'].astype(np.float64)
+#     df['station_id'] = df['station_id'].astype(int)
+#     df[['forecast_tempC', 'forecast_humidity', 'forecast_windSpeed', 'forecast_windDirection',
+#         'pm10']] = scalers.get_scaler(False, "a").fit_transform(
+#         df[['forecast_tempC', 'forecast_humidity', 'forecast_windSpeed', 'forecast_windDirection', 'pm10']])
+#     df = df.append(df.tail(1))
+#     df_pred = utils["create_x"](df[['station_id','date_time','forecast_tempC','forecast_humidity','forecast_windSpeed','forecast_windDirection','pm10']], 72)
+#     df_pred = np.array(df_pred)
+#     preds = model.get_model("1b").predict(df_pred)
+#     return_list = []
+#     for l in preds[0]:
+#         return_list.append(utils["pm10_index"](l[0]))
+#     prediction.update_station_pred(req['station_id'], return_list)
+#     return jsonify(prediction.get_prediction(req['station_id']))
+
+
 @route1.route('/model1b', methods=['POST'])
 def model1b():
     req = request.get_json()
-    tmpDs = np.array(meteo.update_meteo_value(req['station_id'], req['val']))
-    tmpDs = tmpDs[0]
-    df = pd.DataFrame({'station_id': tmpDs[:, 0], 'date_time': tmpDs[:, 1], 'forecast_tempC': tmpDs[:, 2],
-                       'forecast_humidity': tmpDs[:, 3], 'forecast_windSpeed': tmpDs[:, 4],
-                       'forecast_windDirection': tmpDs[:, 5], 'pm10': tmpDs[:, 6]})
-    # cnt = 0
-    # for i in tmpDs[0]:
-    #     j = np.array([[i[2], i[3], i[4], i[5], i[6]]])
-    #     j = scalers.get_scaler(False, "a").fit_transform(j)
-    #     tmpDs[0][cnt] = [i[0], i[1], j[0][0], j[0][1], j[0][2], j[0][3], j[0][4]]
-    #     cnt = cnt + 1
-    # print(tmpDs)
-    # pred = model.get_model("1b").predict(tmpDs)
-    # preds = scalers.get_scaler(False, "b").inverse_transform(pred.reshape(-1, 1)).reshape(pred.shape)
+    req_val = req["val"]
+    station_id = req["station_id"]
+    df = pd.read_csv("routes/NeaSmirni.csv", sep=',', header=True)
+    df = df[['station_id','date_time','forecast_tempC','forecast_humidity','forecast_windSpeed','forecast_windDirection','pm10']]
+    new_row = {'station_id': req[0], 'date_time': req[1], 'forecast_tempC': req[2],
+                        'forecast_humidity': req[3], 'forecast_windSpeed': req[4],
+                        'forecast_windDirection': req[5], 'pm10': req[6]}
+    df = df.append(new_row, ignore_index=True)
+
+    # tmpDs = np.array(meteo.update_meteo_value(req['station_id'], req['val']))
+    # tmpDs = tmpDs[0]
+    # df = pd.DataFrame({'station_id': tmpDs[:, 0], 'date_time': tmpDs[:, 1], 'forecast_tempC': tmpDs[:, 2],
+    #                    'forecast_humidity': tmpDs[:, 3], 'forecast_windSpeed': tmpDs[:, 4],
+    #                    'forecast_windDirection': tmpDs[:, 5], 'pm10': tmpDs[:, 6]})
     df['date_time'] = df['date_time'].astype("|S")
+    df['date_time'] = df['date_time'].map(lambda a: utils["get_time"](a))
     df['date_time'] = df['date_time'].astype(int)
     df['forecast_windDirection'] = df['forecast_windDirection'].astype(np.float64)
     df['station_id'] = df['station_id'].astype(int)
     df[['forecast_tempC', 'forecast_humidity', 'forecast_windSpeed', 'forecast_windDirection',
         'pm10']] = scalers.get_scaler(False, "a").fit_transform(
         df[['forecast_tempC', 'forecast_humidity', 'forecast_windSpeed', 'forecast_windDirection', 'pm10']])
-    df = df.append(df.tail(1))
+    # df = df.append(df.tail(1))
     df_pred = utils["create_x"](df[['station_id','date_time','forecast_tempC','forecast_humidity','forecast_windSpeed','forecast_windDirection','pm10']], 72)
-    df_pred = np.asarray(df_pred).astype(np.float32)
+    # df_pred = np.array(df_pred)
+    df_pred = pd.DataFrame(df_pred, columns=['station_id','date_time','forecast_tempC','forecast_humidity','forecast_windSpeed','forecast_windDirection','pm10'])
     preds = model.get_model("1b").predict(df_pred)
     return_list = []
     for l in preds[0]:
